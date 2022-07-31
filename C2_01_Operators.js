d9bbf507abe3b07e121a1cbf8939c790690e87b8
Shehbaz456 <106955650+Shehbaz456@users.noreply.github.com>
2022-07-31 18:13:30 +0530

    Create C2_01_Operators.js

---
+++ C2_01_Operators.js
@@ -0,0 +1,54 @@
+console.log("Operator in Js");
+let a = 4;
+let b = 3;
+// //Arithmatic Operator
+// // (+ - * / ** % ++ --)
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+// console.log("a ** b = ", a ** b);
+// console.log("a % b = ", a % b);
+// console.log("a++  = ", a++);
+// console.log("a-- = ", a--);
+// console.log("a = ", a);
+
+// //Assignment Operator
+// //(= += -= *= /= %= **=)
+let c = 5;
+let d = 7;
+// c+=1;
+// console.log(c);
+// c-=1;
+// console.log(c);
+// c*=2;
+// console.log(c);
+// c/=2;
+// console.log(c);
+// c**=3;
+// console.log(c);;
+
+// // *** Comparison Operator ***
+// //(== != === !== < > >= <= ?)
+c = 6;
+d = "6";
+// console.log(Boolean(c == d));
+console.log("c == d is ", c == d);
+console.log("c != d is ", c != d);
+console.log("c === d is ", c === d);
+console.log("c !== d is ", c !== d);
+console.log("c >= d is ", c >= d);
+console.log("c <= d is ", c <= d);
+console.log("c < d is ", c < d);
+console.log("c > d is ", c > d);
+
+// logical Operators 
+// (&& || !)
+let x = 5;
+let y = 4;
+console.log(x > y && x == 5);
+console.log(x < y || x == 5);
+console.log(!false);
+console.log(!true);
+
+// ^ done 
